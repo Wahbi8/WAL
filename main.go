4d458c49832c55930a75af3f0f4b98163f8211b1
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-06-26 21:05:48 +0100

    update

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
+type block struct {
+	header header
+	record string // i might change this to byte in the future
+}
 
+type header struct{
+	checkSum uint32 
+	logType uint8
+	lenght uint32
+}
 
-func main() {
+func main()  {
 
 }
