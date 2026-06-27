7d6dd1d7a098b9734bc7debb879ac45599b643d5
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-06-27 18:20:38 +0100

    update

--- main.go
+++ main.go
@@ -1,16 +1,33 @@
 package main
 
+import (
+	"encoding/binary"
+)
+
 type block struct {
 	header header
-	record string // i might change this to byte in the future
+	record record
 }
 
 type header struct{
-	checkSum uint32 
-	logType uint8
-	lenght uint32
+	checkSum uint32	// 4 bytes
+	logType uint8	// 1 byte	// type is (full / start / middle / last) aa a number
+	lenght uint32	// 2 bytes
 }
 
-func main()  {
+type record struct{
+	operation uint8		// 1 byte  // enum (delete / update)
+	keyLength uint8		// 2 bytes 
+	valueLength uint32	// 4 bytes
+	key []byte			// 2 bytes
+	value []byte		// 16 bytes
+}
+
+func main() {
+
+	
+}
+
+func (b *block) serialize() []byte {
 
 }
