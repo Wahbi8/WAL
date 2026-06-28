a28944d37fe89b669c5851032b83a12c1efc6ad1
Oussama_Wahbi <wahbi.oussama08@gmail.com>
2026-06-28 19:22:32 +0100

    update

--- main.go
+++ main.go
@@ -4,23 +4,12 @@ import (
 	"encoding/binary"
 )
 
-type block struct {
-	header header
-	record record
-}
-
-type header struct{
-	checkSum uint32	// 4 bytes
-	logType uint8	// 1 byte	// type is (full / start / middle / last) aa a number
-	lenght uint32	// 2 bytes
-}
-
+// block size 32KB
 type record struct{
-	operation uint8		// 1 byte  // enum (delete / update)
-	keyLength uint8		// 2 bytes 
-	valueLength uint32	// 4 bytes
-	key []byte			// 2 bytes
-	value []byte		// 16 bytes
+	checkSum uint32	// 4 bytes	- fingerprint of the payload
+	logType uint8	// 1 byte	- type is (full / start / middle / last) aa a number
+	lenght uint16	// 2 bytes	- how many bytes is the payload
+	payload []byte  // operation -> keyLength -> key -> value
 }
 
 func main() {
@@ -28,6 +17,15 @@ func main() {
 	
 }
 
-func (b *block) serialize() []byte {
+func (r *record) serialize() []byte {
 
+	// i need to chnage this so it will not exceeds 32KB
+	totalSize := 7 + len(r.payload)
+	buf := make([]byte, totalSize)
+
+	binary.LittleEndian.PutUint32(buf[0:4], r.checkSum)
+	buf[4] = r.logType
+	binary.LittleEndian.PutUint16(buf[5:9], r.lenght)
+
+	
 }
